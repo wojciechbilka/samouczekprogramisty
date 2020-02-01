c967be0c8e9982839527d3bf70539470760e33a3
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-02-01 16:20:50 +0100

    Format by IntelliJ (ctrl + alt + l)

--- src/main/java/BinaryNumbersTutorial/BinaryNumbers.java
+++ src/main/java/BinaryNumbersTutorial/BinaryNumbers.java
@@ -4,25 +4,24 @@ import java.util.ArrayList;
 
 public class BinaryNumbers {
 
-   public static String convertDecimal(int decimal) {
+    public static String convertDecimal(int decimal) {
 
-       StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
 
-       if(decimal < 0) {
-           decimal = Math.abs(decimal);
-           System.out.println("Argument of converDecimal method was negative");
-           throw new IllegalArgumentException();
-       }
-       else if(decimal == 0) {
-           return "0";
-       }
+        if (decimal < 0) {
+            decimal = Math.abs(decimal);
+            System.out.println("Argument of converDecimal method was negative");
+            throw new IllegalArgumentException();
+        } else if (decimal == 0) {
+            return "0";
+        }
 
-       while(decimal > 0) {
-           builder.append(decimal%2);
-           decimal /= 2;
-       }
-       return builder.reverse().toString();
-   }
+        while (decimal > 0) {
+            builder.append(decimal % 2);
+            decimal /= 2;
+        }
+        return builder.reverse().toString();
+    }
 
     public static String convertDecimal(double decimal) {
 
@@ -32,35 +31,32 @@ public class BinaryNumbers {
         int decimalIntegers = (int) decimal;
         double decimalFractional = decimal - decimalIntegers;
 
-        if(decimal < 0) {
+        if (decimal < 0) {
             decimal = Math.abs(decimal);
             System.out.println("Argument of converDecimal method was negative");
             throw new IllegalArgumentException();
-        }
-        else if(decimal == 0) {
+        } else if (decimal == 0) {
             return "0";
         }
 
-        for(int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             fractionalList.add(decimalFractional);
             decimalFractional = round(decimalFractional * 2.0, 3);
 
-            if(decimalFractional > 1) {
+            if (decimalFractional > 1) {
                 builder.append("1");
                 decimalFractional = round(decimalFractional - 1.0, 3);
-            }
-            else if(decimalFractional == 1) {
+            } else if (decimalFractional == 1) {
                 builder.append("1");
                 break;
-            }
-            else if(decimalFractional < 1) {
+            } else if (decimalFractional < 1) {
                 builder.append("0");
             }
 
             if (fractionalList.contains(decimalFractional)) {
                 builder.append(")");
                 int index = fractionalList.indexOf(decimalFractional);
-                builder.insert( index, "(");
+                builder.insert(index, "(");
                 break;
             }
         }
@@ -78,18 +74,18 @@ public class BinaryNumbers {
         return (double) tmp / factor;
     }
 
-   public static void main(String args[]) {
-       int exampleNumber = 3321;
-       System.out.println("Are those two binary numbers the same? \n" + convertDecimal(exampleNumber) +
-               "\n" + Integer.toBinaryString(exampleNumber));
+    public static void main(String args[]) {
+        int exampleNumber = 3321;
+        System.out.println("Are those two binary numbers the same? \n" + convertDecimal(exampleNumber) +
+                "\n" + Integer.toBinaryString(exampleNumber));
 
-       double exampleDoubleNumber = 0.1;
-       System.out.println("\n" + convertDecimal(exampleDoubleNumber));
+        double exampleDoubleNumber = 0.1;
+        System.out.println("\n" + convertDecimal(exampleDoubleNumber));
 
-       ArrayList<Double> test = new ArrayList<>();
+        ArrayList<Double> test = new ArrayList<>();
 
-       double testDobule = 0.3123;
-       System.out.println((testDobule % 1));
+        double testDouble = 0.3123;
+        System.out.println((testDouble % 1));
 
-   }
+    }
 }
