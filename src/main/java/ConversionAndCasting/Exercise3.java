edf6ce38432e30d7c64729de76f78dc0c9efc3fb
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-02-08 23:31:57 +0100

    Exercise 3 from: https://www.samouczekprogramisty.pl/konwersja-i-rzutowanie-w-jezyku-java/

---
+++ src/main/java/ConversionAndCasting/Exercise3.java
@@ -0,0 +1,31 @@
+package ConversionAndCasting;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+        System.out.println("Unboxing excepion:");
+        try {
+            ArrayList<?> list = new ArrayList<>();
+            ArrayList<? super Object> listOfObjectSubtypes = new ArrayList<>();
+            ArrayList listOfObjects = new ArrayList();
+            ArrayList<String> listOfStrings = new ArrayList<String>();
+
+            Object test = new Object();
+
+            listOfObjectSubtypes.add("sds");
+            listOfObjects.add(new Object());
+            listOfStrings.add("asd");
+            listOfStrings = (ArrayList<String>) listOfObjects;
+            listOfStrings.get(0).length();
+
+
+
+        } catch (ClassCastException e) {
+            System.out.println("Wrong casting may be dangerous!");
+            e.printStackTrace();
+        }
+    }
+}
