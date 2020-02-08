8bbfe437afc38fc011832880f02cc3c59583bf03
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-02-08 23:36:56 +0100

    Exercise 4 from: https://www.samouczekprogramisty.pl/konwersja-i-rzutowanie-w-jezyku-java/

--- src/main/java/ConversionAndCasting/Exercise4.java
+++ src/main/java/ConversionAndCasting/Exercise4.java
@@ -1,4 +1,15 @@
 package ConversionAndCasting;
 
+import java.util.Scanner;
+
 public class Exercise4 {
+    //Jak myślisz co otrzymasz przypisując zmienną typu char do zmiennej typu int? Znajdziesz ten numer w tabeli ASCII?
+
+    public static void main(String[] args) {
+
+        char a = 'a';
+
+        System.out.println("\"a\" casted to int is " + (int) a + " which is ASCII number of letter \"a\".");
+
+    }
 }
